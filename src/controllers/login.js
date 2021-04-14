c3e1b31cab10bc8fd965d3a5bcef95f5318f97eb
Otavio Mafessoni <omafessoni@hotmail.com>
2021-04-14 08:54:51 -0300

    Alterações no Login
    Validação de Ativo e melhora na verificação de acesso.

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -2,7 +2,7 @@ const conexao = require("../config/conexao");
 
 module.exports = {
   async VerificarAcesso(user, pass) {
-    const sql = `SELECT ID AS ID, NOME, SENHA FROM OPERADORES WHERE NOME = '${user}' AND SENHA ='${pass}'`;
+    const sql = `SELECT ID_OPERADORES AS ID, NOME, SENHA FROM OPERADORES WHERE NOME = '${user}' AND SENHA ='${pass}' AND ATIVO = 1`;
     const result = await conexao.qry(sql);
     return result.recordset;
   },
--- src/routes/login.js
+++ src/routes/login.js
@@ -6,14 +6,16 @@ const key = process.env.key;
 routes.post("/", async (req, res) => {
   var auth = await login.VerificarAcesso(req.body.user, req.body.pass);
 
-  if (auth.length > 0) {
+  const ID = auth[0].ID ? auth[0].ID : 0;
+
+  if (ID > 0) {
     const id = auth[0].ID;
     const token = jwt.sign({ id }, key, {
       expiresIn: 300, //5min
     });
     return res.json({ auth: true, token: token });
   } else {
-    res.status(500).json({ message: "Login inválido!" });  
+    res.status(500).json({ message: "Login inválido!" });
   }
 });
 
